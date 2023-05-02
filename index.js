897369f2699d956eafadf20db2e9a1eb0e301353
itzsajal <113159177+itzsajal@users.noreply.github.com>
2023-05-02 01:46:27 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function openMoreDetails(actressName) {
+  window.open(`https://www.google.com/search?q=${actressName}+hd+photos`, '_blank');
+}
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+document.querySelector("#hamburger-menu").addEventListener("click", function () {
+  document.querySelector(".myList").classList.toggle("show");
+});;
+const boxes = document.querySelectorAll(".nav-list");
+for (const box of boxes) {
+  box.addEventListener("click", function () {
+    document.querySelector(".myList").classList.toggle("show");
+  })
+}
