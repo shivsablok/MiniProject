1da23a5c71644528e6882f2bcedca7212d9219bd
Shiv Sablok <shivsablok27@gmail.com>
2023-12-10 09:13:58 +0530

    project_Updated

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 let slideIndex = 1;
+let slideshowInterval;
+
 showSlides(slideIndex);
+startSlideshow();
 
 // Next/previous controls
 function plusSlides(n) {
@@ -11,15 +14,27 @@ function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
-function openMoreDetails(actressName) {
-  window.open(`https://www.google.com/search?q=${actressName}+hd+photos`, '_blank');
+// Open more details function
+function openMoreDetails(Name) {
+  window.open(`https://www.google.com/search?q=${Name}+images`, '_blank');
 }
+
+function startSlideshow() {
+  slideshowInterval = setInterval(function () {
+    plusSlides(1);
+  }, 3000); // Change to the desired interval in milliseconds (e.g., 5000 for 5 seconds)
+}
+
+function stopSlideshow() {
+  clearInterval(slideshowInterval);
+}
+
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) { slideIndex = 1 }
-  if (n < 1) { slideIndex = slides.length }
+  if (n > slides.length) { slideIndex = 1; }
+  if (n < 1) { slideIndex = slides.length; }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
@@ -30,6 +45,14 @@ function showSlides(n) {
   dots[slideIndex - 1].className += " active";
 }
 
+// Toggle hamburger menu
 document.querySelector("#hamburger-menu").addEventListener("click", function () {
   document.querySelector(".myList").classList.toggle("show");
-});;
+});
+
+// Pause the slideshow on hover
+// let slideshowContainer = document.querySelector('.slideshow-container');
+// slideshowContainer.addEventListener('mouseenter', stopSlideshow);
+
+// Resume the slideshow on mouse leave
+// slideshowContainer.addEventListener('mouseleave', startSlideshow);
